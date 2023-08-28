e422df9268c1554cfc2a7092f13a261a5151001d
Eunsub Lee <ssamko@kakao.com>
2023-08-28 17:23:53 +0900

    sns.models: ADD Post & Follow

--- lion/sns/models.py
+++ lion/sns/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.content[:8]
+
+
+class Follow(models.Model):
+    follower = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="follower"
+    )
+    following = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="following"
+    )
+
+    def __str__(self):
+        return f"{self.follower} follows {self.following}"
