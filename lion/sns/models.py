712562e8b13d95cccaf61aec9649cb6984b7d62a
Eunsub Lee <ssamko@kakao.com>
2023-09-19 13:22:55 +0900

    sns.models: ADD hidden to Post

--- lion/sns/models.py
+++ lion/sns/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
+    hidden = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
